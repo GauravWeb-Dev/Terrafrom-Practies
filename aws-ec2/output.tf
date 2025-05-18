c007d28a16a3f7fc408595eacd1944e7b9e7f69d
GauravWeb-Dev <gauravsardar27@gmail.com>
2025-05-18 16:59:55 +0530

    add variable file

--- aws-ec2/output.tf
+++ aws-ec2/output.tf
@@ -3,7 +3,7 @@ output "aws_instance_public_ip" {
   value = aws_instance.myserver.public_ip
  
 }
-output "aws_instance_public_ip" {
+output "aws_instance_private_ip" {
 
   value = aws_instance.myserver.private_ip
  
