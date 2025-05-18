01fb00490eacc56d7c5c1fb77712d07587798a15
GauravWeb-Dev <gauravsardar27@gmail.com>
2025-05-18 17:28:55 +0530

    add variable file

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -6,3 +6,10 @@ provider "aws" {
 resource "aws_s3_bucket" "mybucket"{
     bucket = "aws-s3-bucket-app-gaurav"
 }
+
+
+resource "aws_s3_object" "bucket_data" {
+    bucket = aws_s3_bucket.mybucket.bucket
+    source = "myfile.txt"
+    key  = "mydata.txt"
+}
