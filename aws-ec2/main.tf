84ba25d1feba958f59bdec6438ca2da634e73144
GauravWeb-Dev <gauravsardar27@gmail.com>
2025-05-18 16:04:02 +0530

    add aws ec2 main.tf file

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -5,5 +5,18 @@ provider "aws" {
 resource "aws_instance" "myserver" {
   ami      = "ami-0af9569868786b23a"
   instance_type = "t2.micro"
-  key_name = "Gaurav-Key"
+  key_name = "Gaurav-key"
 }
+
+# provider "aws" {
+#     region = "ap-south-1"
+# }
+
+# resource "aws_instance" "myserver2" {
+#   ami =""
+#   instance_type = ""
+#   key_name = "Gaurav-key"
+#   tags ={
+#     Name =""
+#   }
+#   }
