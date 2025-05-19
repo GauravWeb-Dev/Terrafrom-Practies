d0afa1d17d7b424839ff9ac47b7e909c9626eaca
GauravWeb-Dev <gauravsardar27@gmail.com>
2025-05-19 16:59:39 +0530

    host static wensite

--- static-website-terraform/main.tf
+++ static-website-terraform/main.tf
@@ -12,14 +12,14 @@ resource "aws_s3_bucket" "webapp-bucket"{
 }
 
 
-resource "aws_s3_object" "index.html" {
+resource "aws_s3_object" "index_html" {
     bucket = aws_s3_bucket.mywebapp-bucket.bucket
     source = "index.html"
     key  = "index.html"
 
 }
 
-resource "aws_s3_object" "style.css" {
+resource "aws_s3_object" "style_css" {
     bucket = aws_s3_bucket.mywebapp-bucket.bucket
     source = "style.css"
     key  = "style.css"
